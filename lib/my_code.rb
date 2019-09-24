92695242d24e5ee8d44843a2d24f7f760dba9098
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-09-24 15:00:20 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
-# Your Code Here
+def map(source_arr) 
+  new_arr = []
+  source_arr.length.times { |index|
+    new_arr << yield(source_arr[index])
+  }
+  new_arr
+end 
+
+def reduce(source_arr, start_value = 0)
+  new_value = start_value
+  source_arr.length.times { |index|
+    if new_value.is_a?(Integer) ? new_value = yield(source_arr[index], new_value) : new_value = !!yield(source_arr[index], new_value)
+    end
+  }
+  p new_value
+end 
